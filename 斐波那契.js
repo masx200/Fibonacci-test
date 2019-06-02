ba94caf7454a1776bc2ede280366ce74dd38318a
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-02 12:37:04 +0800

    Add files via upload

---
+++ 斐波那契.js
@@ -0,0 +1,30 @@
+var bigInt = require("./BigInteger");
+function getfbnq(num) {
+  num = Math.floor(num);
+  /* Maximum call stack size exceeded */
+  if (num > 5000 && !feibonq[num]) {
+    feibonq[num - 5000] = getfbnq(num - 5000);
+  }
+  num = bigInt(num);
+
+  feibonq[num] =
+    feibonq[num] || bigInt(getfbnq(num - 1)).add(bigInt(getfbnq(num - 1)));
+
+  return feibonq[num];
+}
+var feibonq = [bigInt(0), bigInt(1)];
+
+// console.log("5000",getfbnq(5000));
+// var testnum =124999;
+// console.time("斐波那契" + testnum);
+// console.log(testnum, getfbnq(testnum).toString());
+// console.timeEnd("斐波那契" + testnum);
+// console.log(feibonq);
+/* 如果计算的数太大,可能导致内存溢出 */
+for (let i = 1; i < 184500; i += 500 * Math.random()) {
+  var testnum = Math.floor(i);
+  console.time("斐波那契" + testnum);
+  console.log(testnum, getfbnq(testnum).toString());
+  console.timeEnd("斐波那契" + testnum);
+  //   console.log(feibonq);
+}
