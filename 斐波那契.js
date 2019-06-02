1c84836c72e244688391e5b91e2b7319a53ac998
masx200 <34191203+masx200@users.noreply.github.com>
2019-06-02 13:24:13 +0800

    把结果写入到文件

--- 斐波那契.js
+++ 斐波那契.js
@@ -1,9 +1,10 @@
+var fs = require("fs");
 var bigInt = require("./BigInteger");
 function getfbnq(num) {
   num = Math.floor(num);
   /* Maximum call stack size exceeded */
   if (num > 5000 && !feibonq[num]) {
-    feibonq[num - 5000] = getfbnq(num - 5000);
+    feibonq[num - 5000] = feibonq[num - 5000] || getfbnq(num - 5000);
   }
   num = bigInt(num);
 
@@ -21,10 +22,13 @@ var feibonq = [bigInt(0), bigInt(1)];
 // console.timeEnd("斐波那契" + testnum);
 // console.log(feibonq);
 /* 如果计算的数太大,可能导致内存溢出 */
-for (let i = 1; i < 184500; i += 500 * Math.random()) {
+// for (let i = 1; i < 184500; i += 1000 * Math.random()) {
+for (let i = 1; i < 12450; i += 1000 * Math.random()) {
   var testnum = Math.floor(i);
   console.time("斐波那契" + testnum);
   console.log(testnum, getfbnq(testnum).toString());
   console.timeEnd("斐波那契" + testnum);
   //   console.log(feibonq);
 }
+// console.log(JSON.stringify(feibonq));
+fs.writeFile("./Fibonacci-test-"+new Date().getTime()+".txt",JSON.stringify(feibonq),()=>{})
